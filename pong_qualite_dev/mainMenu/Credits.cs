9bda334432b991210f3fa59e6b5d0dc7e6265671
Théo Tornatore <tornatoretheo@gmail.com>
2025-10-07 18:44:10 +0200

    Commentaires du code

--- pong_qualite_dev/mainMenu/Credits.cs
+++ pong_qualite_dev/mainMenu/Credits.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
 
+// Script de la scène Credits
 public partial class Credits : MarginContainer
 {
+	/*
+	*	Fonction retour en arrière à l'appui de la touche "ui_cancel"
+	*/
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event.IsActionPressed("ui_cancel")) // "ui_cancel" = touche Échap par défaut
--- pong_qualite_dev/mainMenu/Leaderboard.cs
+++ pong_qualite_dev/mainMenu/Leaderboard.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
 
+// Script de la scène Leaderboard
 public partial class Leaderboard : MarginContainer
 {
+	/*
+	*	Fonction retour en arrière à l'appui de la touche "ui_cancel"
+	*/
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event.IsActionPressed("ui_cancel")) // "ui_cancel" = touche Échap par défaut
