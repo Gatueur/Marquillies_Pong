2f571c4e31607f9e3aa120a063056560e58c3baa
Pierrick <gatueur@gmail.com>
2025-10-21 19:38:00 +0200

    correction bugs, ajout vitesse augmente

--- pong_qualite_dev/balle/BalleRigidBody.cs
+++ pong_qualite_dev/balle/BalleRigidBody.cs
@@ -8,13 +8,19 @@ public partial class BalleRigidBody : RigidBody2D
 
 	public override void _Ready()
 	{
-		// Connexion automatique pour détecter les collisions avec d'autres corps
-		BodyEntered += OnBodyEntered;
 
 		// Direction aléatoire au lancement
 		LaunchBall();
-	}
-
+		
+		// Timer pour augmenter la vitesse toutes les 5 secondes
+		var timer = new Timer();
+		timer.WaitTime = 3f;    // 3 secondes
+		timer.Autostart = true;
+		timer.OneShot = false;  // répéter indéfiniment
+		timer.Timeout += OnSpeedUp;
+		AddChild(timer);
+		}
+		
 	private void LaunchBall()
 	{
 		Vector2 dir = new Vector2(
@@ -24,21 +30,21 @@ public partial class BalleRigidBody : RigidBody2D
 
 		LinearVelocity = dir * Speed;
 	}
-
-	public override void _PhysicsProcess(double delta)
+	
+	private void OnSpeedUp()
 	{
-		// Maintient la vitesse constante
-		if (LinearVelocity.Length() != 0)
-		{
+		if(Speed >= 600f) {
+			Speed = 600f;
+			GD.Print("On garde la même vitesse");
+		} else { 
+			Speed *= 1.02f; // +2% toutes les 3s
+		}
+		if (LinearVelocity.Length() > 0) {
 			LinearVelocity = LinearVelocity.Normalized() * Speed;
 		}
+		GD.Print("Vitesse augmentée : " + Speed);
 	}
-
-	private void OnBodyEntered(Node body)
-	{
-			Speed *= 1.05f;
-	}
-
+ 
 	private void AddScore(bool rightPlayer)
 	{
 		// On accède à l’UI pour incrémenter le bon score
@@ -56,22 +62,21 @@ public partial class BalleRigidBody : RigidBody2D
 	}
 
 	public void ResetBall(bool toRight)
-{
-	// Position au centre exact du viewport (X et Y)
-	GlobalPosition = GetViewportRect().Size / 2;
-
-	// Stoppe la balle temporairement
-	LinearVelocity = Vector2.Zero;
-
-	// Relance après 0,5 seconde
-	GetTree().CreateTimer(0.5).Timeout += () =>
 	{
-		// Direction aléatoire : X vers droite ou gauche selon toRight, Y aléatoire
-		Vector2 dir = new Vector2(toRight ? 1 : -1, (float)GD.RandRange(-0.3f, 0.3f)).Normalized();
+		// Position au centre exact du viewport (X et Y)
+		GlobalPosition = GetViewportRect().Size / 2;
 
-		// Applique la vitesse
-		LinearVelocity = dir * Speed;
-	};
-}
+		// Stoppe la balle temporairement
+		LinearVelocity = Vector2.Zero;
 
+		// Relance après 0,5 seconde
+		GetTree().CreateTimer(0.5).Timeout += () =>
+		{
+			// Direction aléatoire : X vers droite ou gauche selon toRight, Y aléatoire
+			Vector2 dir = new Vector2(toRight ? 1 : -1, (float)GD.RandRange(-0.3f, 0.3f)).Normalized();
+
+			// Applique la vitesse
+			LinearVelocity = dir * Speed;
+		};
+	}
 }
